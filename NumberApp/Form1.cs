53d013fd59f68d087c2ddf565abe7ac4ad5fcb2b
AyhanY <AyhanY@AYHAN-HP>
2022-01-07 21:42:42 +0300

    Add project files.

---
+++ NumberApp/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumberApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            TextBox textbox2 = new TextBox();
+        }
+        private string yaziyaCevir(decimal tutar)
+        {
+            string sTutar = tutar.ToString("F2").Replace('.', ','); // Replace('.',',') ondalık ayracının . olma durumu için            
+            string lira = sTutar.Substring(0, sTutar.IndexOf(',')); //tutarın tam kısmı
+            string kurus = sTutar.Substring(sTutar.IndexOf(',') + 1, 2);
+            string yazi = "";
+
+            string[] birler = { "", "bir", "iki", "üç", "dört", "beş", "altı", "yedi", "sekiz", "dokuz" };
+            string[] onlar = { "", "on", "yirmi", "otuz", "kırk", "elli", "altmış", "yetmiş", "seksen", "doksan" };
+            string[] binler = { "katrilyon", "trilyon", "milyar", "milyon", "bin", "" };
+
+            //KATRİLYON'un önüne ekleme yapılarak artırabilir.
+
+            int grupSayisi = 6; //sayıdaki 3'lü grup sayısı. katrilyon içi 6. (1.234,00 daki grup sayısı 2'dir.)
+                                //KATRİLYON'un başına ekleyeceğiniz her değer için grup sayısını artırınız.
+
+            lira = lira.PadLeft(grupSayisi * 3, '0'); //sayının soluna '0' eklenerek sayı 'grup sayısı x 3' basakmaklı yapılıyor.            
+
+            string grupDegeri;
+
+            for (int i = 0; i < grupSayisi * 3; i += 3) //sayı 3'erli gruplar halinde ele alınıyor.
+            {
+                grupDegeri = "";
+
+                if (lira.Substring(i, 1) != "0")
+                    grupDegeri += birler[Convert.ToInt32(lira.Substring(i, 1))] + "yüz"; //yüzler                
+
+                if (grupDegeri == "biryüz") //biryüz düzeltiliyor.
+                    grupDegeri = "yüz";
+
+                grupDegeri += onlar[Convert.ToInt32(lira.Substring(i + 1, 1))]; //onlar
+
+                grupDegeri += birler[Convert.ToInt32(lira.Substring(i + 2, 1))]; //birler                
+
+                if (grupDegeri != "") //binler
+                    grupDegeri += binler[i / 3];
+
+                if (grupDegeri == "birbin") //birbin düzeltiliyor.
+                    grupDegeri = "bin";
+
+                yazi += grupDegeri;
+            }
+
+            if (yazi != "")
+                yazi += " lira, ";
+
+            int yaziUzunlugu = yazi.Length;
+
+            if (kurus.Substring(0, 1) != "0") //kuruş onlar
+                yazi += onlar[Convert.ToInt32(kurus.Substring(0, 1))];
+
+            if (kurus.Substring(1, 1) != "0") //kuruş birler
+                yazi += birler[Convert.ToInt32(kurus.Substring(1, 1))];
+
+            if (yazi.Length > yaziUzunlugu)
+                yazi += " kuruş";
+            else
+                yazi += "";
+
+            return yazi;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = yaziyaCevir(Convert.ToDecimal(textBox1.Text));
+          
+        }
+
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                button1.PerformClick();
+        }
+
+    }
+}
+
